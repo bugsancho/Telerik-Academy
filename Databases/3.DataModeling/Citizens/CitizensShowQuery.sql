a8c4cf778d5e940215182cb81965c31e0b2595f7
bugsancho <bugsancho@gmail.com>
2014-08-22 15:54:42 +0300

    Finished Citizens database for 3.DataModeling

---
+++ Databases/3.DataModeling/Citizens/CitizensShowQuery.sql
@@ -0,0 +1,12 @@
+SELECT
+	 p.FirstName + ' ' + p.LastName AS Name, a.AddressText AS Address, t.Name AS Town, c.Name AS Country, cont.Name AS Continent
+		FROM People p
+			JOIN Addresses a
+				ON p.AddressID = a.AddressID
+			JOIN Towns t							
+				ON a.TownID = t.TownID				
+			JOIN Countries c						
+				ON t.CountryID = c.CountryID		
+			JOIN Continents cont					
+				ON c.ContinentID = cont.ContinentID	
+													
